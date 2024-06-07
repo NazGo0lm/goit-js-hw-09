ca047edf5f7856f22aca679ee4da88077a958206
Nazar Nazarko <starmok20@gmail.com>
2024-06-07 12:09:54 +0300

    addjs

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,56 +1,51 @@
-'use strict';
-console.log('hello form');
-const formData = { email: '', message: '' };
+const formData = {
+    email: "",
+    message: "",
+};
+
 const formEl = document.querySelector('.feedback-form');
-let inputData = formEl.querySelector('input');
-let textareaData = formEl.querySelector('textarea');
-let arrayData = formEl.elements;
-arrayData = Array.from(arrayData);
-arrayData.pop();
-formEl.addEventListener('input', addLocalStorage);
-formEl.addEventListener('submit', submitingForm);
-window.addEventListener('DOMContentLoaded', returnData);
-// adding localstorage data from event input
-function addLocalStorage(event) {
-  event.preventDefault();
-  arrayData.forEach(area => {
-    if (area.name === 'email') {
-      formData.email = area.value.trim();
-    } else if (area.name === 'message') {
-      formData.message = area.value.trim();
-    }
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-  });
-}
-// validating end working form
-function submitingForm(event) {
-  event.preventDefault();
-  let isValid = false;
-  if (inputData.value && textareaData.value) {
-    isValid = true;
-  } else {
-    isValid = false;
-    formEl.reset();
-  }
-  if (!isValid) {
+
+formEl.addEventListener('input', () => {
+  const formData = new FormData(formEl);
+  const email = formData.get('email');
+  const message = formData.get('message');
+  const data = { email, message };
+  saveToLS('formData', data);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const data = loadFromLS('formData');
+  formEl.elements.email.value = data?.email || '';
+  formEl.elements.message.value = data?.message || '';
+});
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(formEl);
+  const email = formData.get('email');
+  const message = formData.get('message');
+  const data = { email, message };
+  if (email === '' || message === '') {
     alert('All form fields must be filled in');
   } else {
-    formData.email = inputData.value;
-    formData.message = textareaData.value;
+    console.log(data);
     formEl.reset();
-    console.log(formData);
+    localStorage.removeItem('formData');
   }
-  localStorage.removeItem('feedback-form-state');
+});
+
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+    localStorage.setItem(key, jsonData);
 }
-// return form data
-function returnData(events) {
-  let savedFormData = {};
-  if (localStorage.getItem('feedback-form-state')) {
-    savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
-    inputData.value = savedFormData.email;
-    textareaData.value = savedFormData.message;
-    return console.log(savedFormData);
-  } else {
-    return console.log('not data from localstorage');
+ 
+function loadFromLS(key) {
+  const json = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(json);
+    return data;
+  }
+  catch {
+   return json;
   }
 }
